15a2c62ca698b3cf1456df73a165667dcdc6929a
Varin <whltaoin@163.com>
2025-11-09 22:18:13 +0800

    将存储到浏览器中的session内容移存到redis中

--- src/main/java/cn/varin/hututu/model/entity/User.java
+++ src/main/java/cn/varin/hututu/model/entity/User.java
@@ -1,11 +1,9 @@
 package cn.varin.hututu.model.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableLogic;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.Date;
 
 /**
@@ -14,7 +12,10 @@ import java.util.Date;
  */
 @TableName(value ="user")
 @Data
-public class User {
+public class User implements Serializable {
+    @TableField(exist = false) // 表示该字段在数据库中不存在
+    private static final long serialVersionUID = 1L;
+
     /**
      * id
      */
